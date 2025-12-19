a9e0dfc6d64dc4ce032be7334590d5723a1c109f
Adepta <144885226+adeptaxiao@users.noreply.github.com>
2025-12-19 21:10:00 +0200

    Update main.rs

--- 3-polymorphism/src/main.rs
+++ 3-polymorphism/src/main.rs
@@ -21,19 +21,15 @@ impl<S: Storage<u64, User>> UserRepositoryStatic<S> {
     pub fn new(storage: S) -> Self {
         Self { storage }
     }
-
     pub fn add(&mut self, user: User) {
         self.storage.set(user.id, user);
     }
-
     pub fn get(&self, id: u64) -> Option<&User> {
         self.storage.get(&id)
     }
-
     pub fn update(&mut self, user: User) {
         self.storage.set(user.id, user);
     }
-
     pub fn remove(&mut self, id: u64) -> Option<User> {
         self.storage.remove(&id)
     }
@@ -47,19 +43,15 @@ impl<'a> UserRepositoryDynamic<'a> {
     pub fn new(storage: &'a mut dyn Storage<u64, User>) -> Self {
         Self { storage }
     }
-
     pub fn add(&mut self, user: User) {
         self.storage.set(user.id, user);
     }
-
     pub fn get(&self, id: u64) -> Option<&User> {
         self.storage.get(&id)
     }
-
     pub fn update(&mut self, user: User) {
         self.storage.set(user.id, user);
     }
-
     pub fn remove(&mut self, id: u64) -> Option<User> {
         self.storage.remove(&id)
     }
@@ -71,9 +63,7 @@ pub struct InMemoryStorage<K, V> {
 
 impl<K: std::cmp::Eq + std::hash::Hash, V> InMemoryStorage<K, V> {
     pub fn new() -> Self {
-        Self {
-            data: HashMap::new(),
-        }
+        Self { data: HashMap::new() }
     }
 }
 
@@ -81,11 +71,9 @@ impl<K: std::cmp::Eq + std::hash::Hash, V> Storage<K, V> for InMemoryStorage<K, 
     fn set(&mut self, key: K, val: V) {
         self.data.insert(key, val);
     }
-
     fn get(&self, key: &K) -> Option<&V> {
         self.data.get(key)
     }
-
     fn remove(&mut self, key: &K) -> Option<V> {
         self.data.remove(key)
     }
@@ -99,11 +87,7 @@ mod tests {
     fn test_static_repo() {
         let storage = InMemoryStorage::new();
         let mut repo = UserRepositoryStatic::new(storage);
-        let user = User {
-            id: 1,
-            email: Cow::from("a@example.com"),
-            activated: true,
-        };
+        let user = User { id: 1, email: Cow::from("a@example.com"), activated: true };
         repo.add(user);
         let got = repo.get(1).unwrap();
         assert_eq!(got.email, "a@example.com");
@@ -116,11 +100,7 @@ mod tests {
     fn test_dynamic_repo() {
         let mut storage = InMemoryStorage::new();
         let mut repo = UserRepositoryDynamic::new(&mut storage);
-        let user = User {
-            id: 2,
-            email: Cow::from("b@example.com"),
-            activated: false,
-        };
+        let user = User { id: 2, email: Cow::from("b@example.com"), activated: false };
         repo.add(user);
         let got = repo.get(2).unwrap();
         assert_eq!(got.email, "b@example.com");
